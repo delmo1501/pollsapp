a04dc9769d9cf49f50fa986a653a203b9dad8761
Bruno <delmo123123@gmail.com>
2023-12-26 22:18:50 +0100

    /polls, /join, /rejoin, last with dummy data

--- server/src/polls/polls.controller.ts
+++ server/src/polls/polls.controller.ts
@@ -1,20 +1,30 @@
-import { Body, Controller, Logger, Post } from "@nestjs/common";
-import { CreatePollDto } from "./dtos";
+import { Body, Controller, Post } from "@nestjs/common";
+import { CreatePollDto, JoinPollDto } from "./dtos";
+import { PollsService } from "./polls.service";
 
 @Controller('polls')
 export class PollsController {
+    constructor(private pollsService: PollsService) {}
+
     @Post()
     async create(@Body() createPollDto: CreatePollDto) {
-        Logger.log('Create!');
+        const result = await this.pollsService.createPoll(createPollDto);
+        return result;
     }
 
     @Post('/join')
-    async join(){
-        Logger.log('Join!');
+    async join(@Body() joinPollDto: JoinPollDto){
+        const result = await this.pollsService.joinPoll(joinPollDto);
+        return result;
     }
 
     @Post('/rejoin')
     async rejoin(){
-        Logger.log('Rejoin!');
+        const result = await this.pollsService.rejoinPoll({
+            name: 'dummy data',
+            pollID: 'Also dummy',
+            userID: 'dummy',
+        });
+        return result;
     }
 }
